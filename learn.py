a64fa044e0353cfb7d98ea67419d7d6d72209e6e
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-06-05 16:10:10 +0000

    print va type ga example bo'ldi.

---
+++ learn.py
@@ -0,0 +1,14 @@
+ism = "Ali"
+yosh = 25
+boy = 1.75
+student = True
+
+print(ism)
+print(yosh)
+print(boy)
+print(student)
+
+print(type(ism))
+print(type(yosh))
+print(type(boy))
+print(type(student))
