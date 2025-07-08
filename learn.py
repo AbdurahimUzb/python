36eb12a35de6358152b3dd7da666cf1aa70b354b
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-07-08 18:41:29 +0500

    set da remove() ga example bo'ldi

--- learn.py
+++ learn.py
@@ -1,10 +1,5 @@
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing'")
-adjective3 = input("Enter an adjective (description): ")
+my_set = {1, 2, 3}
 
-print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, I saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
+my_set.remove(5)
+
+print(my_set)
