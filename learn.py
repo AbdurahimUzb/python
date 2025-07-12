0079727410c9cd2053800a10ab0cefcbe465432e
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-07-12 16:57:09 +0500

    ternar operatorga example bo'ldi

--- learn.py
+++ learn.py
@@ -1,22 +1,2 @@
-def route_info(distance):
-
-    dis = distance.get("distance")
-    speed = distance.get("speed")
-    time = distance.get("time")
-
-    if distance and time:
-        print(
-            f"Distance to your destination is {speed * time}")
-    elif dis:
-        print(f"Distance to your destination {dis}")
-    else:
-        print("No distance info is available")
-
-
-example = {
-    "time": 10,
-    "speed": 2,
-    "distance": "1000 meter",
-}
-
-route_info(example)
+example = 10
+print("in stock" if example > 0 else "out of stoct")
