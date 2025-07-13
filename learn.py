fee6a6f19e65c8a2bf7c97a04fd2a895f8e0e8f2
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-07-13 11:33:40 +0500

    sys.path example

--- learn.py
+++ learn.py
@@ -1,10 +1,5 @@
 import sys
 
-if len(sys.argv) < 2:
-    print("Iltimos argument kiriting!")
-    sys.exit(1)
-    # Xato kodi bilan chiqish
-else:
-    print("Arguments: ", sys.argv[1:])
-    sys.exit(0)
-    # Muvaffaqiyatli yakunlanish
+print("Import yo'llari:")
+for path in sys.path:
+    print(path)
